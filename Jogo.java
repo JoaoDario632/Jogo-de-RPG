a90c3e33aad5ddf0f3657d8dceb37cc946f108d2
ElprogramadorxX <mdarruda@gmail.com>
2025-10-05 21:06:24 -0300

    Adiocando o sistema de armas

--- Jogo.java
+++ Jogo.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 
 public class Jogo {
     private Scanner sc;
+    private Arma arma;
 
     public Jogo(Scanner sc) {
         this.sc = sc;
+    }
+    public Jogo(Scanner sc, Arma arma) {
+        this.sc = sc;
+        this.arma = arma;
     }
 
     public void comecar() {
         System.out.println("\n=== Criação do Personagem ===");
         System.out.print("Digite o nome do seu herói: ");
         String nome = sc.nextLine();
+
         System.out.println("O herói " + nome + " foi criado com sucesso!");
+
+        if (arma != null) {
+            System.out.println("Ele começará sua jornada equipado com: " + arma);
+        }
     }
 }

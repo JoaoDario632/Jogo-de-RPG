a4a33267d66c7e5206697ad03f038a68157195a8
ElprogramadorxX <mdarruda@gmail.com>
2025-10-05 21:43:45 -0300

    Adicionando melhorias no Jogo

--- Jogo.java
+++ Jogo.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
 public class Jogo {
     private Scanner sc;
     private Arma arma;
+    private Armadura armadura;
 
     public Jogo(Scanner sc) {
         this.sc = sc;
     }
+
     public Jogo(Scanner sc, Arma arma) {
         this.sc = sc;
         this.arma = arma;
+    }
+
+    public Jogo(Scanner sc, Arma arma, Armadura armadura) {
+        this.sc = sc;
+        this.arma = arma;
+        this.armadura = armadura;
     }
 
     public void comecar() {
@@ -19,8 +27,22 @@ public class Jogo {
 
         System.out.println("O herói " + nome + " foi criado com sucesso!");
 
-        if (arma != null) {
-            System.out.println("Ele começará sua jornada equipado com: " + arma);
+        if (arma == null) {
+            arma = Arma.escolherArma(sc);
         }
+        if (arma != null) {
+            System.out.println("Ele começará sua jornada equipado com a arma: " 
+                               + arma.getNome() + " (Dano: " + arma.getConstanteDano() + ")");
+        }
+
+        if (armadura == null) {
+            armadura = Armadura.escolherArmadura(sc);
+        }
+        if (armadura != null) {
+            System.out.println("E protegido pela armadura: " 
+                               + armadura.getNome() + " (Defesa: " + armadura.getConstanteDefesa() + ")");
+        }
+
+        System.out.println("\nSua aventura está prestes a começar...");
     }
 }

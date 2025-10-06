f91d017f5a3aef9e2f5046f3a0a2cf577c7c1160
ElprogramadorxX <mdarruda@gmail.com>
2025-10-05 21:05:39 -0300

    Adicionando o sistema de Escolha de arma

--- Arma.java
+++ Arma.java
@@ -1,24 +1,24 @@
-public class Arma extends Equipamento{
-    private int constanteDano;
-    private String categoria;
+    public class Arma extends Equipamento{
+        private int constanteDano;
+        private String categoria;
 
-    public Arma(String nome, int constanteDano, String categoria) {
-        super(nome);
-        setConstanteDano(constanteDano);
-        setCategoria(categoria);
-    }
+        public Arma(String nome, int constanteDano, String categoria) {
+            super(nome);
+            setConstanteDano(constanteDano);
+            setCategoria(categoria);
+        }
 
-    public int getConstanteDano() {
-        return constanteDano;
-    }
-    public void setConstanteDano(int constanteDano) {
-        this.constanteDano = constanteDano;
-    }
+        public int getConstanteDano() {
+            return constanteDano;
+        }
+        public void setConstanteDano(int constanteDano) {
+            this.constanteDano = constanteDano;
+        }
 
-    public String getCategoria() {
-        return categoria;
+        public String getCategoria() {
+            return categoria;
+        }
+        public void setCategoria(String categoria) {
+            this.categoria = categoria;
+        }
     }
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-}

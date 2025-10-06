4890142ab07ced7cd07e66d975c95b4f8584280a
ElprogramadorxX <mdarruda@gmail.com>
2025-10-05 21:42:50 -0300

    Melhorando a parte de Arma

--- Arma.java
+++ Arma.java
@@ -1,24 +1,50 @@
-    public class Arma extends Equipamento{
-        private int constanteDano;
-        private String categoria;
+import java.util.Scanner;
 
-        public Arma(String nome, int constanteDano, String categoria) {
-            super(nome);
-            setConstanteDano(constanteDano);
-            setCategoria(categoria);
-        }
+public class Arma extends Equipamento{
+    private int constanteDano;
+    private String categoria;
 
-        public int getConstanteDano() {
-            return constanteDano;
-        }
-        public void setConstanteDano(int constanteDano) {
-            this.constanteDano = constanteDano;
-        }
+    public Arma(String nome, int constanteDano, String categoria) {
+        super(nome);
+        setConstanteDano(constanteDano);
+        setCategoria(categoria);
+    }
 
-        public String getCategoria() {
-            return categoria;
-        }
-        public void setCategoria(String categoria) {
-            this.categoria = categoria;
+    public int getConstanteDano() {
+        return constanteDano;
+    }
+    public void setConstanteDano(int constanteDano) {
+        this.constanteDano = constanteDano;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+     public static Arma escolherArma(Scanner sc) {
+        System.out.println("\n=== Escolha sua Arma ===");
+        System.out.println("1. Espada (Dano 10, Categoria: Corpo a Corpo)");
+        System.out.println("2. Machado (Dano 12, Categoria: Corpo a Corpo)");
+        System.out.println("3. Arco (Dano 8, Categoria: Distância)");
+        System.out.print("Digite sua escolha: ");
+
+        int escolha = sc.nextInt();
+        sc.nextLine(); 
+
+        switch (escolha) {
+            case 1: return new Arma("Espada", 10, "Corpo a Corpo");
+            case 2: return new Arma("Machado", 12, "Corpo a Corpo");
+            case 3: return new Arma("Arco", 8, "Distância");
+            default:
+                System.out.println("Opção inválida. Você ficará sem arma!");
+                return null;
         }
     }
+
+    @Override
+    public String toString() {
+        return getNome() + " (Dano: " + constanteDano + ", Categoria: " + categoria + ")";
+    }
+}
